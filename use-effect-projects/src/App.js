60024b70d306c5ea760549f781c5667bab64f1fb
kaiisea <abacatemarinheiro@gmail.com>
2022-10-16 04:45:26 +0200

    working in random photo

--- use-effect-projects/src/App.js
+++ use-effect-projects/src/App.js
@@ -1,12 +1,15 @@
 import "./App.css";
 import Clock from "./components/Clock";
 import { Photo } from "./components/Photo";
+import { Work } from "./components/Work";
+
 
 function App() {
   return (
     <div>
       <Clock />
       <Photo />
+      <Work />
     </div>
   );
 }
--- use-effect-projects/src/components/Work.js
+++ use-effect-projects/src/components/Work.js
@@ -1,15 +1,16 @@
 import React, { useEffect, useState } from "react";
 
-export const Photo = () => {
+export const Work = () => {
   const source = "https://picsum.photos/200";
   const [photo, setPhoto] = useState(source);
   useEffect(() => {
     setInterval(() => {
-      setPhoto(photo());
+      setPhoto(photo(count() => count + 1));
     }, 1000);
   }, []);
   return (
     <div>
+        {count}
       <img src={photo} alt="Random photography" width="200" height="200" />
     </div>
   );
